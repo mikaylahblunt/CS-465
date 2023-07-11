0c416652922676e3266663931784b6a64c98336d
Mikaylah Blunt <mikaylahblunt@Mikaylahs-MacBook-Air.local>
2023-07-11 19:19:18 -0400

    new

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs = require('hbs');
 
-var indexRouter = require("./app_server/routes/index");
-var usersRouter = require("./app_server/routes/users");
-var travelRouter = require("./app_server/routes/travel");
+var indexRouter = require('./app_server/routes/index');
+var usersRouter = require('./app_server/routes/users');
+var travelRouter = require('./app_server/routes/travel');
 
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'app_server', 'views'));
 
 hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
 
@@ -27,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use("/travel", travelRouter);
+app.use('/travel', travelRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
