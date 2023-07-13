a7e134962d239e7bd2fb68ebd2d8ca6e8c72b534
Mikaylah Blunt <mikaylahblunt@Mikaylahs-MacBook-Air.local>
2023-07-12 20:36:20 -0400

    module3

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -1,6 +1,6 @@
-const fs = require('fs');
+var fs = require('fs');
 
-const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 const travel = (req, res) => {
     res.render('travel', {title: "Travlr Getaways", trips});
